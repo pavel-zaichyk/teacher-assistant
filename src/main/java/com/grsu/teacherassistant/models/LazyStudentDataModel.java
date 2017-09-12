4c7c8169106134e36a7ec20d80888fc927cc5375
zaychickpavel <zaychick.pav@yandex.ru>
2017-09-12 21:19:44 +0300

    fix student filter

--- src/main/java/com/grsu/teacherassistant/models/LazyStudentDataModel.java
+++ src/main/java/com/grsu/teacherassistant/models/LazyStudentDataModel.java
@@ -3,6 +3,7 @@ package com.grsu.teacherassistant.models;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 /**
@@ -10,77 +11,79 @@ import java.util.*;
  */
 public class LazyStudentDataModel extends LazyDataModel<LessonStudentModel> {
 
-	private List<LessonStudentModel> datasource;
+    private List<LessonStudentModel> datasource;
 
-	public LazyStudentDataModel(List<LessonStudentModel> datasource) {
-		this.datasource = datasource;
-	}
+    public LazyStudentDataModel(List<LessonStudentModel> datasource) {
+        this.datasource = datasource;
+    }
 
-	@Override
-	public LessonStudentModel getRowData(String rowKey) {
-		for (LessonStudentModel student : datasource) {
-			if (student.getId().toString().equals(rowKey))
-				return student;
-		}
+    @Override
+    public LessonStudentModel getRowData(String rowKey) {
+        for (LessonStudentModel student : datasource) {
+            if (student.getId().toString().equals(rowKey))
+                return student;
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public Object getRowKey(LessonStudentModel student) {
-		return student.getId();
-	}
+    @Override
+    public Object getRowKey(LessonStudentModel student) {
+        return student.getId();
+    }
 
-	@Override
-	public List<LessonStudentModel> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-		List<LessonStudentModel> data = new ArrayList<>();
+    @Override
+    public List<LessonStudentModel> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+        List<LessonStudentModel> data = new ArrayList<>();
 
-		//filter
-		for (LessonStudentModel student : datasource) {
-			boolean match = true;
+        //filter
+        for (LessonStudentModel student : datasource) {
+            boolean match = true;
 
-			if (filters != null) {
-				for (Iterator<String> it = filters.keySet().iterator(); it.hasNext(); ) {
-					try {
-						String filterProperty = it.next();
-						Object filterValue = filters.get(filterProperty);
-						String fieldValue = String.valueOf(student.getClass().getField(filterProperty).get(student));
+            if (filters != null) {
+                for (Iterator<String> it = filters.keySet().iterator(); it.hasNext(); ) {
+                    try {
+                        String filterProperty = it.next();
+                        Object filterValue = filters.get(filterProperty);
+                        Field field = student.getClass().getDeclaredField(filterProperty);
+                        field.setAccessible(true);
+                        String fieldValue = String.valueOf(field.get(student));
 
-						if (filterValue == null || fieldValue.toLowerCase().startsWith(filterValue.toString().toLowerCase())) {
-							match = true;
-						} else {
-							match = false;
-							break;
-						}
-					} catch (Exception e) {
-						match = false;
-					}
-				}
-			}
+                        if (filterValue == null || fieldValue.toLowerCase().startsWith(filterValue.toString().toLowerCase())) {
+                            match = true;
+                        } else {
+                            match = false;
+                            break;
+                        }
+                    } catch (Exception e) {
+                        match = false;
+                    }
+                }
+            }
 
-			if (match) {
-				data.add(student);
-			}
-		}
+            if (match) {
+                data.add(student);
+            }
+        }
 
-		//sort
-		if (sortField != null) {
-			Collections.sort(data, new LazyStudentSorter(sortField, sortOrder));
-		}
+        //sort
+        if (sortField != null) {
+            Collections.sort(data, new LazyStudentSorter(sortField, sortOrder));
+        }
 
-		//rowCount
-		int dataSize = data.size();
-		this.setRowCount(dataSize);
+        //rowCount
+        int dataSize = data.size();
+        this.setRowCount(dataSize);
 
-		//paginate
-		if (dataSize > pageSize) {
-			try {
-				return data.subList(first, first + pageSize);
-			} catch (IndexOutOfBoundsException e) {
-				return data.subList(first, first + (dataSize % pageSize));
-			}
-		} else {
-			return data;
-		}
-	}
+        //paginate
+        if (dataSize > pageSize) {
+            try {
+                return data.subList(first, first + pageSize);
+            } catch (IndexOutOfBoundsException e) {
+                return data.subList(first, first + (dataSize % pageSize));
+            }
+        } else {
+            return data;
+        }
+    }
 }
